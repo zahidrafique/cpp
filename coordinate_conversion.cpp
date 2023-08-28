c2d96f8bc4af7412ef483d044699d3cd4747b816
Zahid Rafique <zrafique@lib.usc.edu>
2023-08-28 12:12:15 -0700

    Implemented on Aug 28

--- coordinate_conversion.cpp
+++ coordinate_conversion.cpp
@@ -3,20 +3,30 @@
 
 using namespace std;
 
+const double PI = 3.14159;
+
 /*
  * This program converts cartesian coordinates to polar coordinates
  */
 
 //Todo: Implement getInput() function
-
+void getInput(double&, double&);		//Prototype
 
 //Todo: Implement calculateRadius() function
-
+double calculateRadius(double x, double y) {		//Passing by value
+	return int(sqrt(x * x + pow(y, 2)) * 100) / 100.0;		//Limit to 2 decimal points by 1. multiply by 100, 2. type cast to int, 3. divide by 100.0
+}
 
 //Todo: Implement calculateAngle() function
-
+double calculateAngle(double x, double y) {
+	return atan(y / x);
+}
 
 //Todo: Implement printOutput() function
+void printOutput(double r, double theta) {
+	double degrees = theta * 180 / 3.14159;
+	cout << "r: " << r << ", theta: " << theta << "radians - " << degrees << "degrees" << endl;
+}
 
 //int main() {
 int coordinateConversionMain() {
@@ -24,6 +34,7 @@ int coordinateConversionMain() {
 
 	getInput(x, y);
 
+	//trying to anticipate run time error and handle it
 	if (x == 0) {
 		cout << "Error: value of x cannot be zero!" << endl;
 		return -1;
@@ -35,4 +46,12 @@ int coordinateConversionMain() {
 	printOutput(r, theta);
 
 	return 0;
+}
+
+//Implementation
+void getInput(double& a, double& y) {			//Passing by reference
+	cout << "Enter the value of x: ";
+	cin >> a;
+	cout << "Enter the value of y: ";
+	cin >> y;
 }
