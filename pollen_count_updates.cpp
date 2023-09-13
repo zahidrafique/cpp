2f61b0b965aa672f1dcf20f031cef8e666b8999f
Zahid Rafique <zrafique@lib.usc.edu>
2023-09-13 12:25:12 -0700

    Implement read_data() & accamulate_write_data()

--- pollen_count_updates.cpp
+++ pollen_count_updates.cpp
@@ -12,8 +12,8 @@ using namespace std;
 *	from the file. Additionally, the average of the new file’s data is calculated and displayed
 */
 
-//int main() {
-int PollenReadings() {
+int main() {
+//int PollenReadings() {
 	const int SIZE = 10;
 	int data[SIZE] = { 0 };
 
@@ -33,10 +33,54 @@ int PollenReadings() {
 }
 
 //Todo: Implement read_data()
+void read_data(int data[], int SIZE) {
+	ifstream inFile(FILENAME.c_str());
+	if (inFile.fail()) {
+		cout << "Error: Input File Stream faield to open for " << FILENAME.c_str() << endl;
+		exit(1);
+	}
 
+	int val;
+	int i = 0;
+	while (i < SIZE) {
+		inFile >> val;
+		if (inFile.eof()) {
+			break;
+		}
+
+		data[i++] = val;
+	}
+
+	/*
+	for (int i = 0; i < SIZE; i++) {
+		inFile >> data[i];
+	}
+	*/
+
+	inFile.close();
+}
 
 //Todo: Implement accumulate_write_data()
+int accumulate_write_data(int data[], int SIZE, int new_reading) {
+	ofstream outFile(FILENAME.c_str(), ios::app);
+	if (outFile.fail()) {
+		cout << "Error: Output File Stream faield to open for " << FILENAME.c_str() << endl;
+		exit(1);
+	}
 
+	int sum = 0;
+	for (int i = 1; i < SIZE; i++) {
+		outFile << data[i] << endl;
+		sum += data[i];
+	}
+
+	outFile << new_reading << endl;
+	sum += new_reading;
+
+	outFile.close();
+
+	return sum;
+}
 
 //Todo: Implement namespace 
 
