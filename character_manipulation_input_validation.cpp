08637aa24949d1173d4075bb31b6921488741cff
Zahid Rafique <zrafique@lib.usc.edu>
2023-09-27 11:08:48 -0700

    Implemented input validation for scientific numbers

--- character_manipulation_input_validation.cpp
+++ character_manipulation_input_validation.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
@@ -25,3 +26,68 @@ int chracterManip() {
 
 //Todo: Implement isValidScientificNumber()
 
+bool isValidScientificNumber(string s, double& retVal) {
+	int eIndex = -1;
+
+	//Verify there is only one 'e' or 'E'
+	for (int i = 0; i < s.size(); i++) {
+		if (eIndex < 0 && (s[i] == 'e' || s[i] == 'E')) {	//Found our first e or E
+			eIndex = i;
+			continue;
+		}
+
+		if (eIndex >= 0 && (s[i] == 'e' || s[i] == 'E')) {	//Found a 2nd e or E
+			return false;
+		}
+	}
+
+	if (eIndex < 0) {
+		return false;
+	}
+
+	//Split input string into base & exponent
+	string base = s.substr(0, eIndex);
+	string exponent = s.substr(eIndex + 1);
+
+	//Validate base
+	int bIndex = 0;
+	if (base[bIndex] == '-' || base[bIndex] == '+') {
+		bIndex++;
+	}
+
+	if (!isdigit(base[bIndex])) {
+		return false;
+	}
+	else {
+		bIndex++;
+	}
+
+	if (base[bIndex++] != '.') {
+		return false;
+	}
+
+	for (int i = bIndex; i < base.length(); i++) {
+		if (!isdigit(base[i])) {
+			return false;
+		}
+	}
+
+	//Validate exponent
+	int xIndex = 0;
+	if (exponent[xIndex] == '-' || exponent[xIndex] == '+') {
+		xIndex++;
+	}
+
+	for (int i = xIndex; i < exponent.length(); i++) {
+		if (!isdigit(exponent[i])) {
+			return false;
+		}
+	}
+
+	//Convert base into double, exponent into int and calculate value
+	double baseDouble = atof(base.c_str());
+	int exponentInt = atoi(exponent.c_str());
+	retVal = baseDouble * pow(10, exponentInt);
+
+	return true;
+}
